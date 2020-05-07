f74d6b8013151cd7a834be5aa75fd0c1cde824d7
Nelson Henry <nelsonhenry.user@gmail.com>
2020-05-07 22:01:30 +0200

    body transition

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
-// (() => {
-//   const colorThief = new ColorThief();
-//   const imgs = document.querySelectorAll('.grid__item.color-thief img');
-//   const main = document.querySelector('.main');
+document.addEventListener("DOMContentLoaded", () => document.body.classList.remove('loading'));
+window.addEventListener('beforeunload', () => document.body.classList.add('loading')
 
-//   // Make sure image is finished loading
-//   imgs.forEach(img => {
-//     img.addEventListener('mouseenter', function () {
-//       console.log('hover');
-//       let c = colorThief.getColor(img);
-//       let yiq = ((c[0] * 299) + (c[1] * 587) + (c[2] * 114)) / 1000;
-//       (yiq >= 128) ?
-//       main.style.color = 'black':
-//         main.style.color = 'white';
-//       main.style.background = `rgb(${c[0]}, ${c[1]}, ${c[2]})`;
-//     });
-//     img.addEventListener('mouseleave', () => {
-//       main.style.background = '';
-//       main.style.color = '';
-//     })
-//   })
-// })()
+    // (() => {
+    //   const colorThief = new ColorThief();
+    //   const imgs = document.querySelectorAll('.grid__item.color-thief img');
+    //   const main = document.querySelector('.main');
+
+    //   // Make sure image is finished loading
+    //   imgs.forEach(img => {
+    //     img.addEventListener('mouseenter', function () {
+    //       console.log('hover');
+    //       let c = colorThief.getColor(img);
+    //       let yiq = ((c[0] * 299) + (c[1] * 587) + (c[2] * 114)) / 1000;
+    //       (yiq >= 128) ?
+    //       main.style.color = 'black':
+    //         main.style.color = 'white';
+    //       main.style.background = `rgb(${c[0]}, ${c[1]}, ${c[2]})`;
+    //     });
+    //     img.addEventListener('mouseleave', () => {
+    //       main.style.background = '';
+    //       main.style.color = '';
+    //     })
+    //   })
+    // })()
