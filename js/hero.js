8779094727ad11e27bfe4299b3b0c5b1acf225bd
Nelson Henry <nelsonhenry.user@gmail.com>
2020-05-08 20:28:51 +0200

    flickity dragthreshold fix

--- js/hero.js
+++ js/hero.js
@@ -30,7 +30,6 @@
     lazyLoad: 1,
     groupCells: true,
     wrapAround: true,
-    selectedAttraction: 0.2,
     dragThreshold: 10, // dragging doesn't start until 10px moved
     on: {
       ready() {
