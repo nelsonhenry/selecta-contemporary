546beb979e99fd117ae749e00282228c1104a0ae
Nelson Henry <nelsonhenry.user@gmail.com>
2020-05-08 23:50:18 +0200

    no message

--- js/grid.js
+++ js/grid.js
@@ -1,11 +1,13 @@
 (() => {
-  let msnry = new Masonry('.grid', {
-    itemSelector: '.grid__item',
-    gutter: '.grid__gutter',
-    columnWidth: '.grid__sizer',
-    horizontalOrder: true,
-    percentPosition: true
-  });
-
-  document.addEventListener('lazyloaded', () => msnry.layout());
+  let grid = document.querySelector('.grid')
+  imagesLoaded(grid, function () {
+    let msnry = new Masonry('.grid', {
+      itemSelector: '.grid__item',
+      gutter: '.grid__gutter',
+      columnWidth: '.grid__sizer',
+      horizontalOrder: true,
+      percentPosition: true
+    });
+    document.addEventListener('lazyloaded', () => msnry.layout());
+  })
 })()
