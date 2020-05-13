a91475bc086252209365786d69e6bb6305985b43
Nelson Henry <nelsonhenry.user@gmail.com>
2020-05-13 23:30:33 +0200

    fix grid imagesloaded

--- js/grid.js
+++ js/grid.js
@@ -1,16 +1,13 @@
 (() => {
   let grid = document.querySelector('.grid')
-  // imagesLoaded(grid, function () {
-  let msnry = new Masonry(grid, {
-    itemSelector: '.grid__item',
-    gutter: '.grid__gutter',
-    columnWidth: '.grid__sizer',
-    horizontalOrder: true,
-    percentPosition: true
-  });
-  // })
-
-  imageLoaded(grid).on('progress', () => msnry.layout())
-
-  document.addEventListener('lazyloaded', () => msnry.layout());
+  imagesLoaded(grid, function () {
+    let msnry = new Masonry('.grid', {
+      itemSelector: '.grid__item',
+      gutter: '.grid__gutter',
+      columnWidth: '.grid__sizer',
+      horizontalOrder: true,
+      percentPosition: true
+    });
+    document.addEventListener('lazyloaded', () => msnry.layout());
+  })
 })()
